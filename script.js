4d7fb83ea651fb603a75d0d77fc8661e96b04a2f
deepikasahu-star <deepika.sahu@fnmathlogic.com>
2025-12-23 21:39:28 +0530

    Refactor event tracking function names and parameters

--- script.js
+++ script.js
@@ -6,18 +6,32 @@ function pushEvent(eventName, data) {
     });
 }
 
-// Travel page submit
-function travelSubmit() {
-    pushEvent("travel_form_submit", {
-        page: "travel_info",
-        product: "travel_insurance"
+// Navigation clicks
+function navClick(label) {
+    pushEvent("navigation_click", {
+        link_name: label
     });
 }
 
-// Billing page submit
+// Add policy
+function addToCart() {
+    pushEvent("add_to_cart", {
+        product: "travel_insurance",
+        price: 2999,
+        currency: "INR"
+    });
+}
+
+// Travel form submit
+function travelSubmit() {
+    pushEvent("travel_form_submit", {
+        step: "travel_info"
+    });
+}
+
+// Payment submit
 function paymentSubmit() {
     pushEvent("payment_submit", {
-        page: "billing",
-        payment_method: "credit_card"
+        step: "billing"
     });
 }
